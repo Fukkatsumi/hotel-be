3d427e988555b9c5fa0954896dcd65d646916bb4
EugeneKondratiev <albrus10@mail.ru>
2020-01-21 18:51:55 +0200

    findFreeApartments has been modified, findFreeApartmentsList has been added

--- src/main/java/com/netcracker/hotelbe/entity/ApartmentClassCustom.java
+++ src/main/java/com/netcracker/hotelbe/entity/ApartmentClassCustom.java
@@ -7,34 +7,19 @@ public class ApartmentClassCustom {
     /**
      * @countOfApartments - quantity free apartments according to booked apartmentClass in "Booking" and booked apartments
      * @apartmentClass - unique object
-     * @apatmemtList - list of apartments which are NOT booked in "Booking"
      * @aparmentPriceOnDates - price on dates according to "ApartmentPrice"
      */
     private Integer countOfApartments;
     private ApartmentClass apartmentClass;
-    private List<Apartment> apartmentList;
     private Integer apartmentPriceOnDates;
 
     public ApartmentClassCustom(ApartmentClass apartmentClass) {
         this.apartmentClass = apartmentClass;
         this.countOfApartments = 0;
-        this.apartmentList = new ArrayList<>();
     }
 
     public void setApartmentClass(ApartmentClass apartmentClass) {
         this.apartmentClass = apartmentClass;
-    }
-
-    public List<Apartment> getApartmentList() {
-        return apartmentList;
-    }
-
-    public void setApartmentList(List<Apartment> apartmentList) {
-        this.apartmentList = apartmentList;
-    }
-
-    public void addToApartmentList(Apartment apartment) {
-        this.apartmentList.add(apartment);
     }
 
     public ApartmentClass getApartmentClass() {
